5a86ee3457739c4ba4fbd00490c80797f050240e
Matias <mradzinski@gmail.com>
2018-05-31 14:59:39 -0300

    Initial commit

---
+++ README.md
@@ -0,0 +1,1 @@
+# rn-prompt-android
---
+++ app/src/main/java/io/mradzinki/rnpickerandroid/App.kt
@@ -0,0 +1,9 @@
+package io.mradzinki.rnpickerandroid
+
+import android.app.Application
+
+class App : Application() {
+    override fun onCreate() {
+        super.onCreate()
+    }
+}
